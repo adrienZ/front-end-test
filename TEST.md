0146a0e92ec4a7ce0db15521b13cd76bc94bd5fd
adrienZLeetchi <94057361+adrienZLeetchi@users.noreply.github.com>
2022-09-22 18:45:07 +0200

    init

---
+++ TEST.md
@@ -0,0 +1,7 @@
+# Test Leetchi Front
+
+Bonjour brave chevalier jedi.
+
+Bienvenue sur le test technique front-end developer de Leetchi :)
+
+Votre mission sera composer en 3 objectifs
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-amber-100">
+    <img
+      src="https://c.tenor.com/WuOwfnsLcfYAAAAC/star-wars-obi-wan-kenobi.gif"
+    />
+
+    <div class="rounded-lg border bg-white">
+      <pre>
+
+         {{ data }}
+        </pre
+      >
+
+      <p class="text-7xl">{{ data.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("https://swapi.dev/api/people/1");
+
+console.log(data.value);
+</script>
